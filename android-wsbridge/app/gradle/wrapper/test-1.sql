6cb217cc0b8d886e856d8bb1f28d6924b909a80b
Dwayne Ewens <ewens_inc@hotmail.com>
2021-02-23 00:29:26 -0600

    Gradle 8.1.1

---
+++ android-wsbridge/app/gradle/wrapper/test-1.sql
@@ -0,0 +1,11 @@
+{
+  "name": "MSSQL",
+  "server": "localhost",
+  "driver": "MSSQL",
+  "port": 1433,
+  "database": "test_db",
+  "username": "sa",
+  "askForPassword": false,
+  "password": "root(!)Password",
+  "connectionTimeout": 15,
+}
